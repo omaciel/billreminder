c3f31924f710ec1f6d4eae71c313bba14113f83c
Og B. Maciel <ogmaciel@src.gnome.org>
2006-03-13 22:21:55 +0000

    Added pysqlite functionality and changed the logo
    svn path=/trunk/; revision=14

--- PyGtk#/BillReminder.py
+++ PyGtk#/BillReminder.py
@@ -2,8 +2,6 @@
 #
 # The MIT License
 #
-# Copyright (c) 2006 Og Maciel
-#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of 
 # this software and associated documentation files (the "Software"), to deal in the 
 # Software without restriction, including without limitation the rights to use, copy, 
@@ -21,13 +19,18 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+# BillReminder - Copyright (c) 2006 Og Maciel
+#
 # -*- coding: utf-8 -*-
 import pygtk
 pygtk.require('2.0')
 import gtk, gtk.glade, gobject
-import os
+import os, sys
 import threading
+from pysqlite2 import dbapi2 as sqlite
 
+# Caminho para a pasta do programa
+path= os.path.abspath (os.path.dirname (sys.argv[0]))
 
 class Dialog:
     def __init__(self, name):
@@ -37,9 +40,52 @@ class Dialog:
         })
         
         self.glade.Modal = true;
-        self.glade.Run();
+        self.glade.Show();
 
+class Display:
+    def __init__(self, whereClause):
+        
+        self.glade = gtk.glade.XML('billreminder.glade', 'frmDisplay')
+ 
+        self.frmDisplay = self.glade.get_widget('frmDisplay')
+        
+        self.glade.signal_autoconnect({
+            'on_btnClose_clicked' : self.on_btnClose_clicked
+        })
+        
+        self.format_listview()
+        self.populate_bills()
+        
+    def format_listview(self):
+    
+        # Treeview
+        treeview = self.glade.get_widget('tvBills')
+        
+        self.list = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
+        self.list.clear()
 
+        treeview.set_model(self.list)
+        treeview.append_column(gtk.TreeViewColumn('Payee', gtk.CellRendererText(), text=0))
+        treeview.append_column(gtk.TreeViewColumn('Amount', gtk.CellRendererText(), text=1))
+        treeview.append_column(gtk.TreeViewColumn('Due Date', gtk.CellRendererText(), text=2))
+        
+    def populate_bills(self, whereClause):
+
+        # TODO: Append the where clause if its not null
+        sqlStatement = "SELECT id, payee, amountDue, dueDate FROM bills ORDER BY dueDate, payee"
+        
+	# Fetch all results to be displayed
+	#rs.execute ("SELECT id, payee, amountDue, dueDate FROM bills ORDER BY dueDate, payee")
+	rs.execute(sqlStatement + whereClause)
+	result = rs.fetchall ()
+
+        for id, payee, amountDue, dueDate in result :
+            self.list.append([payee, amountDue, dueDate])
+         
+    # Callbacks
+    def on_btnClose_clicked(self, widget, data = None):		
+	self.frmDisplay.hide()
+	
 class GtkClient:
 
     def __init__(self):
@@ -58,9 +104,23 @@ class GtkClient:
 	gtk.main_quit()
 
     def on_btnDisplay_clicked(self, widget, data = None):
-	Dialog("frmDisplay")
+	Display("")
 
 if __name__ == '__main__':
-    gtk.threads_init()
+   
+    # Arquivo da base de dados
+    #db= '%s/data/BillReminder.db' % path
+    db = 'data/BillReminder.db'
+
+    # Faz conexão com a base de dados ou cria caso não exista
+    if os.path.isfile (db) :
+        cn = sqlite.connect (db, isolation_level=None)
+        rs = cn.cursor ()
+    else :
+	cn = sqlite.connect (db, isolation_level=None)
+	rs = cn.cursor ()
+	rs.execute ("CREATE TABLE bills (id INTEGER PRIMARY KEY, payee VARCHAR(50) NOT NULL, amountDue VARCHAR(10) NOT NULL, dueDate INTEGER NOT NULL, paid CHAR(1) DEFAULT '0' )")
+
+    #gtk.threads_init()
     client = GtkClient()
     gtk.main()
