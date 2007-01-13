b79b5fec82267547bde136a851f409d76c36ad56
Og B. Maciel <ogmaciel@src.gnome.org>
2007-01-13 00:48:45 +0000

    These are the new classes, one for every window/form of the application.
    *  These are the new classes, one for every window/form of the application.
    
    svn path=/trunk/; revision=103

---
+++ billreminder/aboutdialog.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+#
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of 
+# this software and associated documentation files (the "Software"), to deal in the 
+# Software without restriction, including without limitation the rights to use, copy, 
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+# and to permit persons to whom the Software is furnished to do so, subject to the 
+# following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# BillReminder - Copyright (c) 2006, 2007 Og Maciel
+#
+# -*- coding: utf-8 -*-
+
+import sys
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+      pass
+try:
+    import gtk
+    import gtk.glade
+    import os
+    import gobject
+except:
+    sys.exit(1)
+
+class AboutDialog:
+    """ This is the About dialog window """
+    def __init__(self, gladefile):
+
+        #Set the Glade file
+        self.gladefilename = gladefile
+        self.formName = "frmAbout"
+        self.gladefile = gtk.glade.XML(self.gladefilename, self.formName)
+
+    def run(self):
+        """This function will show the aboutDialog"""
+
+        #Get the actual dialog widget
+        frmAbout = self.gladefile.get_widget(self.formName)
+        frmAbout.set_position(gtk.WIN_POS_CENTER)
+        frmAbout.set_modal(True)
+        #run the dialog and store the response      
+        result = frmAbout.run()
+
+        #we are done with the dialog, destroy it
+        frmAbout.destroy()
+
+        #return the result
+        return  result  
---
+++ billreminder/billdialog.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+#
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of 
+# this software and associated documentation files (the "Software"), to deal in the 
+# Software without restriction, including without limitation the rights to use, copy, 
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+# and to permit persons to whom the Software is furnished to do so, subject to the 
+# following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# BillReminder - Copyright (c) 2006, 2007 Og Maciel
+#
+# -*- coding: utf-8 -*-
+
+import sys
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+      pass
+try:
+    import gtk
+    import gtk.glade
+    import os
+    import gobject
+    import time
+    import datetime
+    from bill import Bill
+    from dal import DAL
+    import utils
+except:
+    sys.exit(1)
+
+class BillDialog:
+    """ This is the dialog to add/edit bills """
+
+    def __init__(self, gladefile, bill=None, parent=None):
+        #Set the Glade file
+        self.gladefilename = gladefile
+        self.formName = "frmBillDialog"
+        self.gladefile = gtk.glade.XML(self.gladefilename, self.formName)
+
+        self.frmBillDialog = self.gladefile.get_widget(self.formName)
+        if parent != None:
+            self.frmBillDialog.set_transient_for(parent)
+            self.frmBillDialog.set_icon(parent.get_icon())
+
+        self.txtAmount = self.gladefile.get_widget("txtAmount")
+        self.cCalendar = self.gladefile.get_widget("cCalendar")
+        self.cboPayee = self.gladefile.get_widget("cboPayee")
+        self.txtNotes = self.gladefile.get_widget("txtNotes")
+        self.txtBuffer = self.txtNotes.get_buffer()
+
+        # Populate payees
+        self._populatePayee()
+
+        self.bill = bill
+
+        # If a bill object was passed, go into edit mode
+        if self.bill != None:
+            self.frmBillDialog.set_title("Editing bill '%s'" % bill.Payee )
+            # Format the amount field
+            self.txtAmount.set_text("%0.2f" % bill.AmountDue)
+            # Format the dueDate field
+            dt = datetime.datetime.fromtimestamp(bill.DueDate)
+            self.cCalendar.select_day(dt.day)
+            self.cCalendar.select_month(dt.month -1,dt.year)
+            utils.select_combo_Text(self.cboPayee,bill.Payee)
+            self.txtBuffer.set_text(bill.Notes)
+
+    def run(self):
+        """ This function will show the dialog """        
+        #run the dialog and store the response      
+        result = self.frmBillDialog.run()
+
+        #we are done with the dialog, destroy it
+        self.frmBillDialog.destroy()
+
+        if (result == gtk.RESPONSE_OK):
+            # Extracts the date off the calendar widget
+            day = self.cCalendar.get_date()[2]
+            month = self.cCalendar.get_date()[1] + 1
+            year = self.cCalendar.get_date()[0]
+            # Create datetime object
+            selectedDate = datetime.datetime(year, month, day)
+            # Turn it into a time object
+            selectedDate = time.mktime(selectedDate.timetuple())
+
+            #buffer = self.txtNotes.get_buffer()
+            startiter, enditer = self.txtBuffer.get_bounds()
+            sbuffer = self.txtBuffer.get_text(startiter, enditer)
+
+            # Gets the payee
+            payee = self._getPayee()
+
+            if self.bill == None:
+                # Create a new object
+                self.bill = Bill(payee, selectedDate, self.txtAmount.get_text(), sbuffer)
+            else:
+                # Edit existing bill
+                self.bill.Payee = payee
+                self.bill.DueDate = selectedDate
+                self.bill.AmountDue = self.txtAmount.get_text()
+                self.bill.Notes = sbuffer
+
+            #return the result and bill
+            return result, self.bill
+        else:
+            #return the result and bill
+            return None, None
+
+    def _populatePayee(self):
+        """ Populates combobox with existing payees """
+        # Connects to the database
+        dal = DAL()
+
+        # List of payees from database
+        payees = dal.Payees()
+
+        store = gtk.ListStore(gobject.TYPE_STRING)
+        for payee in payees:
+            store.append([payee])
+
+        self.cboPayee.set_model(store)
+        self.cboPayee.set_text_column(0)
+        self.cboPayeeEntry = self.cboPayee.child
+        self.selectedText = ''
+
+    def _getPayee(self):
+        """ Extracts information typed into comboboxentry """
+        if self.cboPayee.get_active_iter() != None:
+            model = self.cboPayee.get_model()
+            iteration = self.cboPayee.get_active_iter()
+            if iteration:
+                return model.get_value(iteration, 0)
+        else:
+            return self.cboPayeeEntry.get_text()
