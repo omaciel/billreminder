4c418543990316aa80fb77149a3d9f00502e901f
Og B. Maciel <ogmaciel@src.gnome.org>
2007-03-05 16:01:47 +0000

    Allows to "test run" the application without having to copy files over to
    *  Allows to "test run" the application without having to copy files over to final directory destination.
    
    svn path=/trunk/; revision=141

--- src/common.py
+++ src/common.py
@@ -3,8 +3,11 @@
 
 import os
 
-DEFAULT_PATH = '/usr/share/billreminder/'
-#DEFAULT_PATH ='../gui/'
+if os.path.exists('../gui/'):
+    DEFAULT_PATH = '../gui/'
+else:
+    DEFAULT_PATH = '/usr/share/billreminder/'
+
 #Main window constants
 MAINGLADEFILE = os.path.join(DEFAULT_PATH, 'maindialog.glade')
 MAINFORM_NAME = 'frmMain'
@@ -22,6 +25,6 @@ TRAYGLADEFILE = os.path.join(DEFAULT_PATH, 'systraymenu.glade')
 TRAY_NAME = 'TrayMenu'
 
 #media path
-IMAGE_PATH = os.path.join(DEFAULT_PATH, 'pixmaps/')
+IMAGE_PATH = os.path.join(DEFAULT_PATH, '../images/')
 APP_ICON = os.path.join(IMAGE_PATH, 'billreminder.png')
 APP_HEADER = os.path.join(IMAGE_PATH, 'header.png')
