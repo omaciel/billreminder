c6417e880011403a9bbcba4cbf987f5a694014e0
x-larmesto <x-larmesto@localhost>
2007-03-16 23:50:00 +0000

    svn path=/trunk/; revision=166
    svn path=/trunk/; revision=166

--- src/startup.py
+++ src/startup.py
@@ -2,6 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+
+current_path = os.path.realpath(__file__)
+basedir = os.path.dirname(os.path.realpath(__file__))
+if not os.path.exists(os.path.join(basedir, "startup.py")):
+    if os.path.exists(os.path.join(os.getcwd(), "startup.py")):
+        basedir = os.getcwd()
+sys.path.insert(0, basedir)
+os.chdir(basedir)
+
 try:
     import pygtk
     pygtk.require("2.0")
