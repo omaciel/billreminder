c7b0c9bdc578cb783a518ab76071b787f81e0aa2
Og B. Maciel <ogmaciel@src.gnome.org>
2007-03-23 22:12:46 +0000

    Simplified notification message, limiting it to display only how many
    *  Simplified notification message, limiting it to display only how many outstanding bills are due;
    
    svn path=/trunk/; revision=202

--- src/controller/daemon.py
+++ src/controller/daemon.py
@@ -19,11 +19,11 @@ import controller.trayicon
 from model.dbus_manager import DaemonDBus
 
 class Daemon(model.daemon.Daemon):
-    
+
     def __init__(self):
         model.daemon.Daemon.__init__(self)
         self.__loop()
-        
+
         # Verify if daemon is already running.
         try:
             session_bus = dbus.SessionBus()
@@ -34,16 +34,15 @@ class Daemon(model.daemon.Daemon):
                 return
         except:
             pass
-            
+
         self.dbus_service = DaemonDBus(self)   
-         
+
         self.__set_alarm()
-        
+
         # Keep daemon running
         self.mainloop = gobject.MainLoop()
         self.mainloop.run()
 
-    
     def __getBills(self, cat):
         date = None
         msg = ''
@@ -56,35 +55,31 @@ class Daemon(model.daemon.Daemon):
             msg = '%(msg)s%(item)s\n' % dict(msg=msg, item=item)
             date = i['dueDate']
         return msg
-    
+
     def createNotification(self):
         today = time.mktime(datetime.date.today().timetuple())
         limit = today + (86400 * float(self.config.get('Notification.Days')))
-        notdue = self.dal.get('tblbills', 'dueDate > %(today)s AND dueDate <= %(limit)s AND paid == 0 ORDER BY dueDate' % dict(today=today, limit=limit))
-        duetoday = self.dal.get('tblbills', 'dueDate == %s AND paid == 0' % today)
-        due = self.dal.get('tblbills', 'dueDate < %s AND paid == 0' % today)
-            
+        due = self.dal.get('tblbills', 'dueDate <= %s AND paid == 0' % today)
+
         msg = ''
-            
-        if len(duetoday) > 0: msg += _('<b><big>Bills Due Today</big></b>')
-        msg += self.__getBills(duetoday)
-            
-        if len(due) > 0: msg += '\n' + _('<b><big>Bills Due</big></b>')
-        msg += self.__getBills(due)
-        
-        if len(notdue) > 0: msg += '\n' + _('<b><big>Bills Not Due</big></b>')
-        msg += self.__getBills(notdue)
-        
+
+        if len(due) == 1:
+            msg += '\n' + _('You have 1 outstanding bill to pay!')
+        elif len(due) > 1:
+            msg += '\n' + _('You have %s outstanding bills to pay!' % len(due))
+
+        #msg += self.__getBills(due)
+
         if msg != '': self.showMessage(_('BillReminder'), msg)
-        
+
     def __loop(self):
         self.createNotification()
         gobject.timeout_add(int(self.config.get('Notification.Interval')) * 1000, self.__loop)
-        
+
     def __alarm(self):
         self.createNotification()
         self.__set_alarm()
-    
+
     def __set_alarm(self):
         a_hour, a_min  = self.config.get('Notification.Alarm').split(':')
         a_time = int((float(a_hour) + (float(a_min) / 60.0)) * 3600.0)
@@ -95,8 +90,7 @@ class Daemon(model.daemon.Daemon):
         if _time <= 0: _time += 86400
         print _time
         gobject.timeout_add(_time * 1000, self.__alarm)
-    
-    
+
     def showMessage(self, title, msg):
         try:
             session_bus = dbus.SessionBus()
@@ -116,4 +110,3 @@ class Daemon(model.daemon.Daemon):
             notif.Icon(os.path.abspath(common.APP_HEADER))
             notif.Timeout(20)
             notif.Notify() 
-        
