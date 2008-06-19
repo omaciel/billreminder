fe7e380d9fd888c44cf4cdf888397606f5f01e38
Og B. Maciel <ogmaciel@src.gnome.org>
2008-06-19 15:36:42 +0000

    Set default tag for sysvars.py
    svn path=/trunk/; revision=571

--- autogen.sh
+++ autogen.sh
@@ -4,38 +4,19 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="billreminder"
-REQUIRED_AUTOCONF_VERSION=2.60
-REQUIRED_AUTOMAKE_VERSION=1.9.2
+PKG_NAME=billreminder
 
 (test -f $srcdir/configure.ac \
-  && test -f $srcdir/autogen.sh) || {
+  && test -f $srcdir/autogen.sh \
+  && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-DIE=0
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
 
-gnome_autogen=
-gnome_datadir=
-
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  test -z "$dir" && dir=.
-  if test -f $dir/gnome-autogen.sh ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
-IFS="$ifs_save"
-
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
-  exit 1
-fi
-
-GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
-
+REQUIRED_AUTOMAKE_VERSION=1.9.2 GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
