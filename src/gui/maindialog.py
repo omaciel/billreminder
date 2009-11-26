882c6081f9fb4d6a957ce243ec8752b1a3575b46
Og B. Maciel <ogmaciel@gnome.org>
2009-11-26 15:47:33 -0500

    Handle categories with None

--- src/gui/maindialog.py
+++ src/gui/maindialog.py
@@ -312,7 +312,7 @@ class MainDialog:
         try:
             records = self.actions.get_monthly_totals(start, end)
             # Chart widget takes data in format (('CategoryName', amount),)
-            categories = [cat for cat,total in records]
+            categories = ['None' if not cat else cat for cat,total in records]
             totals = [float(total) for cat,total in records]
             #records = [(c if c else 'None',float(t)) for c,t in records]
         except Exception, e:
